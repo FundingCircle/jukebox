cd6590a36dd6f5f2e60a6bb07addc98832983b8f
matthias-margush <matthias.margush@fundingcircle.com>
2019-02-07 10:55:00 -0800

    Upgrade to cucumber 4.2.2

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/fundingcircle/jukebox/"
   :license {:name "BSD 3-clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[io.cucumber/cucumber-core "4.2.0"]
-                 [io.cucumber/cucumber-junit "4.2.0"]
-                 [org.clojure/clojure "1.10.0" :scope "provided"]
+  :dependencies [[io.cucumber/cucumber-core "4.2.2"]
+                 [io.cucumber/cucumber-junit "4.2.2"]
+                 [org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.1"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [venantius/yagni "0.1.7"]]
