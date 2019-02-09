c17ce39189679a4895b8fcc59c578c35abeecdf7
matthias-margush <matthias.margush@fundingcircle.com>
2019-02-08 17:41:44 -0800

    Update `resource/inventory` to use cucumber tags

--- src/fundingcircle/jukebox/resource.clj
+++ src/fundingcircle/jukebox/resource.clj
@@ -1,10 +1,26 @@
 (ns fundingcircle.jukebox.resource
   "Library for creating an inventory of resources needed to run tests."
   (:require [fundingcircle.jukebox :as jukebox]
+            [fundingcircle.jukebox.backend.cucumber :as cucumber]
             [yagni.graph :as graph]
             [yagni.jvm :as jvm]
             [yagni.namespace :as namesp]
-            [yagni.namespace.form :as form]))
+            [yagni.namespace.form :as form])
+  (:import cucumber.runtime.FeaturePathFeatureSupplier
+           cucumber.runtime.io.MultiLoader
+           cucumber.runtime.model.FeatureLoader))
+
+(defn surrounding-step?
+  "Checks whether a var is a surrounding step fn (:scene/before, :scene/after,
+  :scene/before-step, or :scene/after-step)."
+  [v]
+  (let [step? (->> (meta v)
+                   (keys)
+                   (some #{:scene/before
+                           :scene/after
+                           :scene/before-step
+                           :scene/after-step}))]
+    (when step? v)))
 
 (defn- flattened-call-graph
   "Given a step definition, returns the set of all functions that are
@@ -43,22 +59,24 @@
     (when-not (empty? (select-keys m keys))
       m)))
 
-(defn- count-vars-in-ns
-  "Calls `yagni.namespace.form/count-vars-in-ns`, ignoring unparsable
-  namespaces."
-  [graph ns]
-  ;; The `potemkin` library attempts to "fix" various clojure problems
-  ;; in ways that break the `macro-expand-1` call that `yagni`
-  ;; makes. Ignoring namespaces that are built with potemkin (or are
-  ;; unparsable for other reasons) seems fine for the moment, for the
-  ;; purposes of the feature this code is providing.
-  (try
-    (form/count-vars-in-ns graph ns)
-    (catch Throwable _)))
+(defn ignore-exceptions
+  [f]
+  (fn [& args]
+    (try
+      (apply f args)
+      (catch Throwable _))))
+
+;; The `potemkin` library attempts to "fix" various clojure problems
+;; in ways that break the `macro-expand-1` call that `yagni`
+;; makes. (Code that this is scanning can use potemkin.) Ignoring
+;; namespaces that are built with potemkin (or are unparsable for
+;; other reasons) seems fine for the moment, for the purpose of the
+;; feature this code is providing.
+(alter-var-root #'form/count-vars-in-ns ignore-exceptions)
 
 (defn- call-graph
   "Returns the call graph of every var in each of the namespaces."
-  [namespaces]
+  [namespaces entry-points]
   ;; This is the same as `yagni.core/construct-reference-graph`, but
   ;; with a couple of tweaks:
   ;;  - Instead of acting on a directory, it acts on a list of namespaces
@@ -66,11 +84,11 @@
   (let [namespaces (->> (map ->sym namespaces)
                         (remove #{'clojure.core}))
         graph (atom (namesp/named-vars-map namespaces))
-        generator-fns (jvm/find-generator-fns graph)]
+        generator-fns (jvm/find-generator-fns graph)
+        entry-points (map ->var entry-points)]
     (jvm/extend-generators! graph generator-fns)
-    ;; (form/count-vars graph namespaces)
-    (doall (map (partial count-vars-in-ns graph) namespaces))
-    (graph/prune-findable-nodes! graph nil (atom #{}))
+    (form/count-vars graph namespaces)
+    (graph/prune-findable-nodes! graph entry-points (atom #{}))
     (jvm/compress-generators! graph generator-fns)
     @graph))
 
@@ -79,18 +97,66 @@
   [call-graph keys]
   (fn [[f]]
     (when (jukebox/scene-related? (->var f))
-      (when-let [fcg (flattened-call-graph f call-graph)]
+      (let [fcg (flattened-call-graph f call-graph)]
         (let [r (resources f keys)]
           [f (into (if r #{r} #{})
                    (keep #(resources % keys) fcg))])))))
+(defn parse-tags
+  "Parses the `--tags` command line option."
+  [cli-args]
+  (cucumber.runtime.RuntimeOptions. cli-args))
+
+(defn- pickle-steps
+  "Returns the list of `gherkin.pickle.PickleStep`s in the features
+  matching `tags`."
+  [tags]
+  (let [tags (or tags (cucumber.runtime.RuntimeOptions. []))
+        filters (cucumber.runtime.filter.Filters. tags)]
+   (->>
+    (-> (Thread/currentThread)
+        (.getContextClassLoader)
+        (MultiLoader.)
+        (FeatureLoader.)
+        (FeaturePathFeatureSupplier. tags)
+        (.get))
+    (mapcat #(.getPickles %))
+    (filter #(.matchesFilters filters %))
+    (mapcat #(.getSteps (.pickle %))))))
+
+(defn- step-definition-for-pickle-step
+  "Returns the step-definition that matches the `pickle-step."
+  [step-definitions pickle-step]
+  (:step-fn (first (filter #(.matchedArguments % pickle-step) step-definitions))))
+
+(defn- step-definitions
+  "Returns the step-definitions (clojure fn) matching `tags`."
+  [tags]
+  (let [definitions (cucumber/->definitions)
+        hooks (jukebox/hooks)]
+    (jukebox/register (cucumber/->CucumberJukeBackend definitions) hooks)
+
+    (let [step-definitions
+          (map
+           (fn [{:keys [pattern step-fn]}]
+             (cucumber/->JukeStepDefinition pattern step-fn (meta step-fn)))
+           (:steps @definitions))]
+
+      (->> (pickle-steps tags)
+           (map #(step-definition-for-pickle-step step-definitions %))
+           (concat (filter surrounding-step? hooks))
+           (map ->sym)
+           (into #{})))))
 
 (defn inventory
-  "Returns a map of entry points to inventory manifest.
-
-  Scans the paths for fn's with meta containing `keys` as entry
-  points. Then scans each's entrypoint's call graph transitively,
-  looking for fns also tagged with `keys` in their 'meta'."
-  [keys]
-  (let [cg (call-graph (all-ns))]
+  "Returns a map of entry points to inventory manifest for features matching `tags` (see `parse-tags`)."
+  [keys tags]
+  (let [entry-points (step-definitions tags)
+        cg (call-graph (all-ns) entry-points)
+        entry-points (into #{} (map symbol entry-points))]
     (->> (keep (manifest cg keys) cg)
+         (map (fn [[k v]]
+                [k (into #{} (map #(select-keys % keys) v))]))
+         (filter (fn [[k v]]
+                   (and (entry-points (->sym (resolve k)))
+                        (not= #{} v))))
          (into {}))))
--- test/fundingcircle/jukebox/resource_test.clj
+++ test/fundingcircle/jukebox/resource_test.clj
@@ -1,7 +1,7 @@
 (ns fundingcircle.jukebox.resource-test
   "Test resource inventory libary."
-  (:require [fundingcircle.jukebox.resource :as resource]
-            [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [fundingcircle.jukebox.resource :as resource]))
 
 ;; These helper functions set up a call graph for the test below to cover these scenarios:
 ;; * The inventory should include resources on transitive functions
@@ -16,57 +16,19 @@
 ;;   --> helper-fn-e
 ;;     --> helper-fn-e (recursive scenario)
 
-(defn- helper-fn-a
-  "A test helper fn."
-  {:scene/resource-type :kafka-topic
-   :scene/resource-caps {:topic-name "topic-name-a"}}
-  [])
-
-(defn- helper-fn-b
-  "A test helper fn."
-  {:scene/resource-type :kafka-topic
-   :scene/resource-caps {:topic-name "topic-name-b"}}
-  [])
-
-(defn- helper-fn-c
-  "A test helper fn."
-  {:scene/resource-type :kafka-topic
-   :scene/resource-caps {:topic-name "topic-name-c"}}
-  [])
-
-(defn- helper-fn-d
-  "A test helper fn."
-  {:scene/resource-type :kafka-topic
-   :scene/resource-caps {:topic-name "topic-name-a"}}
-  []
-  (helper-fn-c))
-
-(defn- helper-fn-e
-  "A test helper fn."
-  []
-  (helper-fn-e))
-
-(defn entry-point
-  "A test entry point."
-  {:scene/step "test entry point"
-   :scene/resource-type :kafka-topic
-   :scene/resource-caps {:topic-name "topic-name"}}
-  [_]
-  (helper-fn-a)
-  (helper-fn-b)
-  (helper-fn-d)
-  (helper-fn-e))
+(require 'example.belly) ;; be sure the instrumented fns are loaded
 
 (deftest inventory-test
-  (is (= #{{:scene/resource-type :kafka-topic
-            :scene/resource-caps {:topic-name "topic-name"}}
-           {:scene/resource-type :kafka-topic
-            :scene/resource-caps {:topic-name "topic-name-c"}}
-           {:scene/resource-type :kafka-topic
-            :scene/resource-caps {:topic-name "topic-name-a"}}
-           {:scene/resource-type :kafka-topic
-            :scene/resource-caps {:topic-name "topic-name-b"}}}
-         (->> (resource/inventory [:scene/resource-type])
-              (`entry-point)
-              (map #(select-keys % [:scene/resource-type :scene/resource-caps]))
-              (into #{})))))
+  (testing "resource inventory"
+    
+    (let [inventory (resource/inventory [:scene/resources]
+                                        (resource/parse-tags ["test/features"]))]
+      (is (= {'example.belly/i-have-cukes-in-my-belly
+              #{{:scene/resources [:kafka/topic "topic-name"]}
+                {:scene/resources [:kafka/topic "topic-name-a"]}
+                {:scene/resources [:kafka/topic "topic-name-b"]}
+                {:scene/resources [:kafka/topic "topic-name-c"]}}
+              'example.belly/before-step
+              #{{:scene/resources [:kafka/topic "topic-name-d"]}}}
+             inventory)))))
+
