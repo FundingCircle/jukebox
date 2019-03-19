e6980877a2643cd0685af21495889cd4f28c4c0d
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-03-19 09:37:41 +0000

    upgrade clojure to 1.10

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/fundingcircle/jukebox/"
   :license {:name "BSD 3-clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[io.cucumber/cucumber-core "4.2.0"]
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [io.cucumber/cucumber-core "4.2.0"]
                  [io.cucumber/cucumber-junit "4.2.0"]
-                 [org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.1"]
                  [org.clojure/tools.namespace "0.2.11"]]
   :profiles {:dev {:aliases {"cucumber" ["run" "-m" "cucumber.api.cli.Main"
